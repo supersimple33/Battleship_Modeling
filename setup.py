26669bc7a3a8f02131a053db5695a42cd631196b
addison-hanrattie <private>
2020-09-02 20:08:33 -0400

    Compiled Gym

--- setup.py
+++ setup.py
@@ -1,2 +1,2 @@
 from setuptools import setup
-setup(name='gym_battleship1', version='0.0.1', install_requires=['gym']) # And any other dependencies foo needs
+setup(name='gym_battleship1', version='0.2.0', install_requires=['gym']) # And any other dependencies foo needs maybe ENUM
