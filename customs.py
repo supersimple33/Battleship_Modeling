d2341c907ccf37221e569e8273b352d7da12d6bf
addison-hanrattie <private>
2020-09-20 11:34:06 -0400

    Added hyperparameters

--- customs.py
+++ customs.py
@@ -1,17 +1,20 @@
 import tensorflow as tf
+from tensorflow.keras.layers import InputLayer, Flatten, Dense, Reshape, GlobalMaxPool1D, Reshape, LocallyConnected1D, Add, Activation
 import tensorflow.keras.backend as K
+
+import kerastuner as kt
 
 @tf.function
 def customAccuracy(y_true, y_pred):
-    trans = tf.transpose([y_true, y_pred],perm=[1,0,2])
-    accur = 0 
-    i = 0
-    for elem in trans:
-        move = tf.argmax(elem[1],-1)
-        v = elem[0][move]
-        accur += 1 if v == 1 else 0
-        i += 1
-    return accur / i
+	trans = tf.transpose([y_true, y_pred],perm=[1,0,2])
+	accur = 0 
+	i = 0
+	for elem in trans:
+		move = tf.argmax(elem[1],-1)
+		v = elem[0][move]
+		accur += 1 if v == 1 else 0
+		i += 1
+	return accur / i
 
 @tf.function
 def customLoss(y_true, y_pred):
@@ -20,32 +23,45 @@ def customLoss(y_true, y_pred):
 
 @tf.function
 def f1_loss(y_true, y_pred):
-    tp = K.sum(K.cast(y_true*y_pred, 'float'), axis=0)
-    tn = K.sum(K.cast((1-y_true)*(1-y_pred), 'float'), axis=0)
-    fp = K.sum(K.cast((1-y_true)*y_pred, 'float'), axis=0)
-    fn = K.sum(K.cast(y_true*(1-y_pred), 'float'), axis=0)
+	tp = K.sum(K.cast(y_true*y_pred, 'float'), axis=0)
+	tn = K.sum(K.cast((1-y_true)*(1-y_pred), 'float'), axis=0)
+	fp = K.sum(K.cast((1-y_true)*y_pred, 'float'), axis=0)
+	fn = K.sum(K.cast(y_true*(1-y_pred), 'float'), axis=0)
 
-    p = tp / (tp + fp + K.epsilon())
-    r = tp / (tp + fn + K.epsilon())
+	p = tp / (tp + fp + K.epsilon())
+	r = tp / (tp + fn + K.epsilon())
 
-    f1 = 2*p*r / (p+r+K.epsilon())
-    f1 = tf.where(tf.math.is_nan(f1), tf.zeros_like(f1), f1)
-    return 1 - K.mean(f1)
+	f1 = 2*p*r / (p+r+K.epsilon())
+	f1 = tf.where(tf.math.is_nan(f1), tf.zeros_like(f1), f1)
+	return 1 - K.mean(f1)
 
-def buildModel():
-    inputLay = tf.keras.Input(shape=(10,10,6))
-    f = Flatten()(inputLay)
-    d1 = Dense(150,activation='relu')(f)
-    d2 = Dense(100)(d1)
+def buildModel(hp):
+	m = tf.keras.Sequential()
+	m.add(InputLayer(input_shape=(10,10,6)))
+	m.add(Flatten())
+	for i in range(hp.Int('num_layers', 1, 5)):
+		m.add(Dense(units=hp.Int('units_' + str(i),
+					min_value=32,
+					max_value=512,
+					step=32),
+					activation='relu'))
+	m.add(Dense(100, activation='sigmoid'))
+	return m
 
-    r1 = Reshape((100,6))(inputLay)
-    a = GlobalMaxPool1D('channels_first')(r1)
-    r2 = Reshape((100,1))(a)
-    lc = LocallyConnected1D(1,1)(r2)
-    f2 = Flatten()(lc)
-    # gm1 = GlobalMaxPool2D('channels_first')(inputLay)
-    # gm2 = GlobalMaxPool1D('channels_first')(gm1)
+def buildModel2():
+	inputLay = tf.keras.Input(shape=(10,10,6))
+	f = Flatten()(inputLay)
+	d1 = Dense(150,activation='relu')(f)
+	d2 = Dense(100)(d1)
 
-    a = Add()([d2,f2])
-    out = Activation('sigmoid')(a)
-    return tf.keras.Model(inputs=inputLay, outputs=out)
+	r1 = Reshape((100,6))(inputLay)
+	a = GlobalMaxPool1D('channels_first')(r1)
+	r2 = Reshape((100,1))(a)
+	lc = LocallyConnected1D(1,1)(r2)
+	f2 = Flatten()(lc)
+	# gm1 = GlobalMaxPool2D('channels_first')(inputLay)
+	# gm2 = GlobalMaxPool1D('channels_first')(gm1)
+
+	a = Add()([d2,f2])
+	out = Activation('sigmoid')(a)
+	return tf.keras.Model(inputs=inputLay, outputs=out)
