81162ae2ed789980f3bd54ea4938b8c15a8fa92c
addison-hanrattie <private>
2020-09-25 16:13:38 -0400

    Updated To Numpy SaveLoad

--- trainer.py
+++ trainer.py
@@ -16,6 +16,7 @@ import gym_battleship1
 import builtins
 # import timeit # DEBUG Only
 import time
+from random import randint
 # from collections import deques
 
 from customs import customAccuracy
@@ -30,7 +31,7 @@ LEARNING_RATE = 0.01
 MOMENTUM = 0.05
 CHANNEL_TYPE = "channels_last"
 AXIS = 1 if CHANNEL_TYPE == "channels_first" else -1
-
+# print(NUM_GAMES)
 # CONFIGURING THE MODEL
 
 # model = buildModel()
@@ -57,18 +58,18 @@ print(model.summary())
 ct = time.time()
 env = gym.make('battleship1-v1')
 
-@tf.function(experimental_compile=True) # Decoration is 10 fold faster
-def makeMove(obs,e):
-	# print("Traced with " + str(e))
-	if e > 0:
-		r = tf.random.uniform(shape=[],dtype=tf.dtypes.float16)
-		if r < EPSILON:
-			return tf.random.uniform(shape=[],maxval=100,dtype=tf.dtypes.int64)
-		else:
-			preds = model(obs, training=False)
-			return tf.argmax(preds, 1)[0]
-	preds = model(obs, training=False)
-	return tf.argmax(preds, 1)[0]
+# @tf.function(experimental_compile=True) # Decoration is 10 fold faster
+# def makeMove(obs,e):
+# 	# print("Traced with " + str(e))
+# 	if e > 0:
+# 		r = tf.random.uniform(shape=[],dtype=tf.dtypes.float16)
+# 		if r < EPSILON:
+# 			return tf.random.uniform(shape=[],maxval=100,dtype=tf.dtypes.int64)
+# 		else:
+# 			preds = model(obs, training=False)
+# 			return tf.argmax(preds, 1)[0]
+# 	preds = model(obs, training=False)
+# 	return tf.argmax(preds, 1)[0]
 
 # @tf.function(experimental_compile=True)
 def trainGrads(feature,expect):
@@ -98,29 +99,31 @@ for epoch in range(0,NUM_GAMES):
 	prevOut = np.copy(prevOut) # could get rid of copy in replace of tensor conversion
 	# prevObs = np.moveaxis(prevObs,0,-1) # CPU ONLY
 	prevObs = vfunc(prevObs) # redo timeit with numpy
-	prevObs = tf.convert_to_tensor([prevObs])
+# 	prevObs = tf.convert_to_tensor([prevObs])
 
 	done = False
 
 	slotsLeft = np.ones(shape=100,dtype=np.float32)
 	while not done:
 		# Could Accelerate this, however few tf methods, and a couple of outside methods
-		move = makeMove(prevObs,EPSILON).numpy()
+		# move = makeMove(prevObs,EPSILON).numpy()
+		move = randint(0,99)
 		# print(move.numpy())
 		obs, reward, done, out = env.step(move)
 		obs = vfunc(obs) # numpy may be faster
 
 		if reward:
 			hits += 1
-		prevOut = [t.value[0] for t in prevOut] # look at the last input
+		prevOut = vfunc(prevOut)
 
-		observations.append(prevObs[0])
+		observations.append(prevObs)
 		# expecteds.append(tf.convert_to_tensor(slotsLeft))
 		# slotsLeft[move] = 0
-		expecteds.append(tf.cast(tf.convert_to_tensor(prevOut),dtype=tf.dtypes.float32))
+		expecteds.append(prevOut.astype(float))
+# 		expecteds.append(tf.cast(tf.convert_to_tensor(prevOut),dtype=tf.dtypes.float32))
 
 		# obs = np.moveaxis(obs,0,-1) # CPU ONLY
-		prevObs = tf.convert_to_tensor([obs])
+# 		prevObs = tf.convert_to_tensor([obs])
 		prevOut = np.copy(out)
 		iterartions += 1
 		if done:
@@ -170,17 +173,20 @@ for epoch in range(0,NUM_GAMES):
 
 observations = np.array(observations)
 expecteds = np.array(expecteds)
+# print(observations)
 
 with open('data.npz', 'wb') as f:
+	print('started save')
 	np.savez_compressed(f, observations, expecteds)
-# with open('data.npz', 'rb') as f:
-# 	l = np.load(f)
-# 	pass
+	print('saved')
+with open('data.npz', 'rb') as f:
+	l = np.load(f)
+	pass
 
 # dataset = dataset.batch(32)
 # dataset = dataset.shuffle(dataset.__len__(), reshuffle_each_iteration=True)
-tf.data.experimental.save(dataset=dataset,path='saved_data',compression='GZIP')
+# tf.data.experimental.save(dataset=dataset,path='saved_data',compression='GZIP')
 # with tf.device('/cpu:0'):
 # 	model.fit(x=observationStack,y=expectedStack,epochs=10,verbose=2,callbacks=[tensorboard_callback],use_multiprocessing=True) # multiprocessing?
-model.save('saved_model/my_model')
-print("Model Saved")
+# model.save('saved_model/my_model')
+# print("Model Saved")
--- tuneTraining.py
+++ tuneTraining.py
@@ -30,12 +30,24 @@ else:
 
 # GET DATA
 spec = (tf.TensorSpec(shape=(6, 10, 10), dtype=tf.int32, name=None), tf.TensorSpec(shape=(100,), dtype=tf.float32, name=None))
-dataset = tf.data.experimental.load('saved_data',spec,compression='GZIP')
+# dataset = tf.data.experimental.load('saved_data',spec,compression='GZIP')
+
+with open('data.npz', 'rb') as f:
+	l = np.load(f)
+# 	nA = tf.convert_to_tensor(l['arr_0'])
+# 	nB = tf.convert_to_tensor(l['arr_1'])
+	nA = l['arr_0'] #tf.stack(
+	nB = l['arr_1']
+	l.close()
+# dA = tf.data.Dataset.from_tensor_slices(nA)
+dataset = tf.data.Dataset.from_tensor_slices((nA,nB))
+
+dataset.shuffle(1000, seed=tf.constant(0, dtype=tf.int64))
 dataset = dataset.batch(32)
-trainDataset = dataset.take(4500)
-validationData = dataset.skip(4500)
-# validationData = validationData.take(619)
-# dataset = dataset.repeat(2)
+trainDataset = dataset.take(7500)
+validationData = dataset.skip(7501)
+validationData = validationData.take(750)
+dataset = dataset.repeat()
 
 # npIter = tfds.as_numpy(dataset)
 
@@ -60,13 +72,13 @@ def custModel(hp):
 obj = kt.Objective("val_customAccuracy", direction="max")
 xmodel = kt.applications.xception.HyperXception(include_top=True, input_shape=(6,10,10), classes=100)
 # tuner = kt.RandomSearch(custModel, objective=obj, max_trials=30,executions_per_trial=2,project_name="rstuner")
-tuner = kt.Hyperband(xmodel, max_epochs=8, factor=2, hyperband_iterations=4, objective=obj, distribution_strategy=strategy, project_name="hbtuner", metrics=['mae', customAccuracy], loss='binary_crossentropy') #max_trials=40, max_epochs=6, executions_per_trial=2,
+tuner = kt.Hyperband(xmodel, max_epochs=8, factor=2, hyperband_iterations=3, objective=obj, distribution_strategy=strategy, project_name="hbtuner", metrics=['mae', customAccuracy], loss='binary_crossentropy') #max_trials=40, max_epochs=6, executions_per_trial=2,
 
 # with tf.device('CPU:2'):
 # 	print(f"Starting training with {len(trainDataset)} batches")
 
 tuner.search_space_summary()
-tuner.search(trainDataset, validation_data=validationData, shuffle=True, use_multiprocessing=True, workers=4, verbose=1) #epochs
+tuner.search(trainDataset, validation_data=validationData, shuffle=True, use_multiprocessing=True, workers=4, verbose=2) #epochs
 tuner.results_summary() #224,384,224,100 #0.001
 
 model = tuner.get_best_models(num_models=2)[0]
