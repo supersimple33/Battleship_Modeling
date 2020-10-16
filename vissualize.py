5580e5815b48cf29713486b43b8994228e4f9bd9
addison-hanrattie <private>
2020-10-16 15:52:51 -0400

    changes some names

--- vissualize.py
+++ vissualize.py
@@ -5,16 +5,13 @@ import tensorflow as tf
 import h5py
 import numpy as np
 
-import copy
-
 import tensorflow.keras.losses
 tensorflow.keras.losses.custom_loss = tf.nn.sigmoid_cross_entropy_with_logits
 
+tf.keras.backend.set_image_data_format('channels_last')
 from customs import customAccuracy, buildModel
 
 from matplotlib import pyplot
-
-tf.keras.backend.set_image_data_format('channels_last')
 
 env = gym.make('battleship1-v1')
 env.reset()
@@ -30,15 +27,17 @@ if tensorflow.keras.backend.image_data_format() == "channels_last":
 	LCWeights = [LC, model.layers[-2].get_weights()[1]]
 	model.layers[-2].set_weights(LCWeights)
 
-print()
 def displayKernel(lay, ch=None):
+	if model.layers[lay].use_bias:
+		filters, biases = model.layers[lay].get_weights()
+	else:
+		filters = model.layers[lay].get_weights()[0]
 	if ch is None:
-		chRange = range(6)
+		chRange = range(min([2,int(filters.shape[2])]))
 	else:
 		chRange = range(ch[0], ch[1])
 	pyplot.close()
 	# retrieve weights from the second hidden layer
-	filters, biases = model.layers[lay].get_weights()
 	# normalize filter values to 0-1 so we can visualize them
 	f_min, f_max = filters.min(), filters.max()
 	filters = (filters - f_min) / (f_max - f_min)
@@ -50,7 +49,7 @@ def displayKernel(lay, ch=None):
 		# plot each channel separately
 		for j in chRange:
 			# specify subplot and turn of axis
-			ax = pyplot.subplot(n_filters, ch[1]-ch[0], ix)
+			ax = pyplot.subplot(n_filters, len(chRange), ix)
 			ax.set_xticks([])
 			ax.set_yticks([])
 			# plot filter channel in grayscale
@@ -59,6 +58,15 @@ def displayKernel(lay, ch=None):
 	# show the figure
 	pyplot.show()
 
+# displayKernel(-2)
+def scaler(x):
+	if x.value[1] == "!M!":
+		return -2
+	elif '(' in x.value[1]:
+		return 2
+	return x.value[0]
+
+vfunc = np.vectorize(scaler)
 def heatMap(y_preds, state=None):
 # 	pyplot.close()
 	y_preds = np.reshape(y_preds, (10,10))
@@ -66,8 +74,9 @@ def heatMap(y_preds, state=None):
 	pyplot.imshow(y_preds, cmap='gray')
 	if state is not None:
 		bx = pyplot.subplot(1,2,2)
-		x = [[(x.value[0] if x.value[1] != "!M!" else -2) for x in y] for y in state]
-		pyplot.imshow(x, cmap='gray')
+		x = np.array(state)
+		x = vfunc(x)
+		pyplot.imshow(x, cmap='gray', vmin=-2, vmax=2)
 	pyplot.show()
 
 scores = []
@@ -103,7 +112,8 @@ for each_game in range(1):
 		prev_obs = new_observation
 		prev_obs = [[[x.value[0] for x in y] for y in c] for c in prev_obs] # redo timeit with numpy
 		prev_obs = tf.convert_to_tensor([prev_obs])
-		prev_obs = tf.reshape(tf.transpose(prev_obs[0]),shape=(1,10,10,6)) # ONLY NEEDED FOR CPUS
+		if tensorflow.keras.backend.image_data_format() == "channels_last":
+			prev_obs = tf.reshape(tf.transpose(prev_obs[0]),shape=(1,10,10,2)) # ONLY NEEDED FOR CPUS
 
 		prev_action = action
 		if done:
