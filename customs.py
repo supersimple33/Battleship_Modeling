5810d16e3b6651eec0659a655ccf29bb99efe9d2
supersimple33 <40609224+supersimple33@users.noreply.github.com>
2021-07-26 23:07:53 -0400

    Added New Constraint

--- customs.py
+++ customs.py
@@ -2,9 +2,71 @@ import tensorflow as tf
 from tensorflow.keras.layers import InputLayer, Flatten, Dense, Reshape, GlobalMaxPool1D, Reshape, LocallyConnected2D, Add, Activation, Conv2D, BatchNormalization, LeakyReLU, MaxPool2D, AveragePooling2D, Concatenate, Lambda, LayerNormalization, SeparableConv2D
 import tensorflow.keras.backend as K
 
+from tensorflow.python.ops import array_ops
+
 # import kerastuner as kt
 
 AXIS = 1 if K.image_data_format() == "channels_first" else -1
+
+class TransformConstaint(tf.keras.constraints.Constraint):
+
+	def __call__(self, w):
+		w_shape = w.shape
+		if w_shape.rank is None or w_shape.rank != 4:
+			raise ValueError('The weight tensor must be of rank 4, but is of shape: %s' % w_shape)
+		
+		height, width, channels, kernels = w_shape
+		w = K.reshape(w, (height, width, channels * kernels))
+
+		# Map the constraint on the kernel
+		w = K.stack(array_ops.unstack(w, axis=-1), axis=0)
+		w = tf.map_fn(self._kernel_constraint, w)
+		return K.reshape(K.stack(array_ops.unstack(w, axis=0), axis=-1), (height, width, channels, kernels))
+
+	def _kernel_constraint(self, kernel):
+		kernel_shape = K.shape(kernel)[0] # assumes square tensor
+
+		# Copy all the values in the diagonals
+		i = tf.constant(0)
+		while_condition = lambda i, _: tf.less(i, kernel_shape // 2)
+		def diag(i, array):
+			selection = array[i, i]
+			indexes = [[i, (kernel_shape - 1) - i], [(kernel_shape - 1) - i, i], [(kernel_shape - 1) - i, (kernel_shape - 1) - i]]
+
+			moddedArr = tf.tensor_scatter_nd_update(array, indexes, tf.fill([3], selection))
+			
+			return [i + 1, moddedArr]
+
+		_, kernel = tf.while_loop(while_condition, diag, [i, kernel])
+
+		# Copy all the values in along the middle axes
+		def cross(i, array):
+			selection = array[i, kernel_shape // 2]
+			indexes = [[kernel_shape // 2, i], [kernel_shape // 2, (kernel_shape - 1) - i], [(kernel_shape - 1) - i, kernel_shape // 2]]
+
+			moddedArr = tf.tensor_scatter_nd_update(array, indexes, tf.fill([3], selection))
+
+			return [i + 1, moddedArr]
+
+		_, kernel = tf.while_loop(while_condition, cross, [i, kernel])
+
+		# Nesting these loops probably does not help in terms of overhead time, however its more intuitive
+		i = tf.constant(1)
+		def outer(i, array): # move up the kernel
+			j = tf.constant(0) # might be faster to just copy this?
+			inner_condition = lambda j, _: tf.less(j, i)
+			def inner(j, innerArr): # move in the kernel
+				selection = innerArr[(kernel_shape // 2) - i - 1, (kernel_shape // 2) - j - 1]
+				indexes = [[(kernel_shape // 2) - j - 1, (kernel_shape // 2) - i - 1], [(kernel_shape // 2) + j + 1, (kernel_shape // 2) - i - 1], [(kernel_shape // 2) + i + 1, (kernel_shape // 2) - j - 1], [(kernel_shape // 2) + i + 1, (kernel_shape // 2) + j + 1], [(kernel_shape // 2) + j + 1, (kernel_shape // 2) + i + 1], [(kernel_shape // 2) - j - 1, (kernel_shape // 2) + i + 1], [(kernel_shape // 2) - i - 1, (kernel_shape // 2) + j + 1]]
+				
+				retArr = tf.tensor_scatter_nd_update(innerArr, indexes, tf.fill([7], selection))
+				return [j + 1, retArr]
+			_, moddedArr = tf.while_loop(inner_condition, inner, [j, array])
+			return [i + 1, moddedArr]
+
+		_, kernel = tf.while_loop(while_condition, outer, [i, kernel])
+
+		return kernel # return necessary?
 
 @tf.function
 def customAccuracy(y_true, y_pred):
@@ -42,6 +104,8 @@ def buildModel():
 	reg2 = tf.keras.regularizers.L1L2(0.0,0.0001)
 	reg1 = None
 
+	constraint = TransformConstaint()
+
 	numFilters = 16
 	activation = 'selu'
 
@@ -49,11 +113,11 @@ def buildModel():
 
 	c1 = Conv2D(filters=numFilters // 2,kernel_size=1,activation=activation, padding='same')(inputLay) # DESTORY/REPLACE
 
-	c3 = Conv2D(filters=numFilters, kernel_size=3, activation=activation, padding='same', kernel_regularizer=reg1)(inputLay)
+	c3 = Conv2D(filters=numFilters // 2, kernel_size=3, activation=activation, padding='same', kernel_regularizer=reg1, kernel_constraint=constraint)(inputLay)
 
 	c5 = Conv2D(filters=numFilters, kernel_size=5, activation=activation, padding='same', kernel_regularizer=reg1)(inputLay) # separable
 
-	c7 = Conv2D(filters=numFilters // 2, kernel_size=3, activation=activation, padding='same', dilation_rate=3, kernel_regularizer=reg1)(inputLay) # Separable
+	c7 = Conv2D(filters=numFilters // 2, kernel_size=3, activation=activation, padding='same', dilation_rate=3, kernel_regularizer=reg1, kernel_constraint=constraint)(inputLay) # Separable
 
 	ap = AveragePooling2D(3, 1, padding='same')(inputLay) # what should we look at misses?
 	ap = Conv2D(filters=numFilters // 2, kernel_size=3, activation=activation, padding='same', kernel_regularizer=reg1)(ap)
